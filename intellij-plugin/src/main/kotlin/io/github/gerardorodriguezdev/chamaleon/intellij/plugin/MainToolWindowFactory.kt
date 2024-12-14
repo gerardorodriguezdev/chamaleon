73f28b3907f487f2843608b693bd78279aadd475
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2024-12-14 13:27:42 +0000

    Basic toolwindow setup

--- intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/MainToolWindowFactory.kt
+++ intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/MainToolWindowFactory.kt
@@ -1,14 +1,25 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin
 
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import org.jetbrains.jewel.bridge.addComposeTab
+import org.jetbrains.jewel.bridge.theme.SwingBridgeTheme
+import org.jetbrains.jewel.foundation.ExperimentalJewelApi
+import org.jetbrains.jewel.ui.component.TextField
 
 class MainToolWindowFactory : ToolWindowFactory {
+    @OptIn(ExperimentalJewelApi::class)
     override fun createToolWindowContent(
         project: Project,
         toolWindow: ToolWindow
     ) {
-
+        toolWindow.addComposeTab("Chamaleon") {
+            SwingBridgeTheme {
+                val textFieldState = rememberTextFieldState("Something")
+                TextField(state = textFieldState)
+            }
+        }
     }
 }
