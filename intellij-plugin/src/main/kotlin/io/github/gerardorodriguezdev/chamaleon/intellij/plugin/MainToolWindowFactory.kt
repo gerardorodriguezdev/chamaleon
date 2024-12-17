d2f59c63f908161a0435f295ccd19b4151c630df
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2024-12-17 21:26:43 +0000

    Updates MainToolWindowFactory.kt

--- intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/MainToolWindowFactory.kt
+++ intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/MainToolWindowFactory.kt
@@ -1,13 +1,13 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin
 
-import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.layout.Column
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import org.jetbrains.jewel.bridge.addComposeTab
 import org.jetbrains.jewel.bridge.theme.SwingBridgeTheme
 import org.jetbrains.jewel.foundation.ExperimentalJewelApi
-import org.jetbrains.jewel.ui.component.TextField
+import org.jetbrains.jewel.ui.component.VerticallyScrollableContainer
 
 class MainToolWindowFactory : ToolWindowFactory {
     @OptIn(ExperimentalJewelApi::class)
@@ -17,8 +17,11 @@ class MainToolWindowFactory : ToolWindowFactory {
     ) {
         toolWindow.addComposeTab("Chamaleon") {
             SwingBridgeTheme {
-                val textFieldState = rememberTextFieldState("Something")
-                TextField(state = textFieldState)
+                VerticallyScrollableContainer {
+                    Column {
+
+                    }
+                }
             }
         }
     }
