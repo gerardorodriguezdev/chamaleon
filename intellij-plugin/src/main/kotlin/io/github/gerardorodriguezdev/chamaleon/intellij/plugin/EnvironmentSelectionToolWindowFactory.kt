fba43eb96f9ac841c201a943242136d5f34aa230
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2024-12-19 17:35:06 +0000

    Removes code on ToolWindow

--- intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/EnvironmentSelectionToolWindowFactory.kt
+++ intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/EnvironmentSelectionToolWindowFactory.kt
@@ -1,9 +1,14 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin
 
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import io.github.gerardorodriguezdev.chamaleon.core.DefaultEnvironmentsProcessor
 import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.messages.Bundle
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.presentation.EnvironmentSelectionPresenter
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.EnvironmentSelection
+import kotlinx.coroutines.Dispatchers
 import org.jetbrains.jewel.bridge.addComposeTab
 import org.jetbrains.jewel.foundation.ExperimentalJewelApi
 
@@ -13,8 +18,19 @@ class EnvironmentSelectionToolWindowFactory : ToolWindowFactory {
         project: Project,
         toolWindow: ToolWindow
     ) {
+        val presenter = EnvironmentSelectionPresenter(
+            environmentsProcessor = DefaultEnvironmentsProcessor(),
+            uiDispatcher = Dispatchers.EDT,
+            backgroundDispatcher = Dispatchers.IO,
+        )
+
         toolWindow.addComposeTab(Bundle.environmentSelectionWindowName) {
-            //TODO: Connect
+            EnvironmentSelection(
+                state = presenter.state.value,
+                onEnvironmentChanged = { environmentPath, newSelectedEnvironment ->
+                    presenter.onEnvironmentChanged(project.basePath, environmentPath, newSelectedEnvironment)
+                },
+            )
         }
     }
 }
