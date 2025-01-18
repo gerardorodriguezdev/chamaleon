a3b341e7d8bb0f991ed7fd47507d0ae78eb098e1
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-01-18 12:18:53 +0000

    Adds file type configuration

---
+++ intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/fileTypes/ChamaleonFileType.kt
@@ -0,0 +1,21 @@
+package io.github.gerardorodriguezdev.chamaleon.intellij.plugin.fileTypes
+
+import com.intellij.json.JsonLanguage
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.NlsSafe
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.icons.Icons
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.messages.Bundle
+import org.jetbrains.annotations.NonNls
+import javax.swing.Icon
+
+class ChamaleonFileType private constructor() : LanguageFileType(JsonLanguage.INSTANCE, true) {
+    override fun getName(): @NonNls String = "Chamaleon"
+
+    override fun getDescription(): @NlsContexts.Label String = Bundle.fileTypeForChamaleonConfigFiles
+
+    override fun getDefaultExtension(): @NlsSafe String = "json"
+
+    override fun getIcon(): Icon? = Icons.pluginIcon
+
+}
---
+++ intellij-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/icons/Icons.kt
@@ -0,0 +1,8 @@
+package io.github.gerardorodriguezdev.chamaleon.intellij.plugin.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val pluginIcon = IconLoader.getIcon("/images/expui/toolWindow/pluginIcon.svg", javaClass)
+}
