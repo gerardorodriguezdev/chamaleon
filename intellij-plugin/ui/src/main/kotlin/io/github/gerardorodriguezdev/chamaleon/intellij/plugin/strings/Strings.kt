b49a966628c345e6ca8eab9c9e86b08f4e624af6
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-02-05 17:20:08 +0000

    Updates strings

--- intellij-plugin/ui/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/strings/Strings.kt
+++ intellij-plugin/ui/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/strings/Strings.kt
@@ -12,12 +12,14 @@ interface Strings {
     val previous: String
     val next: String
     val finish: String
+    val selectEnvironmentsDirectoryLocation: String
+    val environmentsDirectoryLocation: String
 }
 
 object DefaultStrings : Strings {
     override val environmentSelectionWindowName: String = "Environment selection"
-    override val environmentsDirectoryPath: String = "Environments directory path"
-    override val selectedEnvironment: String = "Selected environment"
+    override val environmentsDirectoryPath: String = "Environments directory path:"
+    override val selectedEnvironment: String = "Selected environment:"
     override val removeSelectedEnvironment: String = "Remove selected environment"
     override val refreshEnvironments: String = "Refresh environments"
     override val fileTypeForChamaleonConfigFiles: String = "File type for Chamaleon config files"
@@ -26,4 +28,6 @@ object DefaultStrings : Strings {
     override val previous: String = "Previous"
     override val next: String = "Next"
     override val finish: String = "Finish"
+    override val selectEnvironmentsDirectoryLocation: String = "Select the environments directory location"
+    override val environmentsDirectoryLocation: String = "Environments directory location:"
 }
