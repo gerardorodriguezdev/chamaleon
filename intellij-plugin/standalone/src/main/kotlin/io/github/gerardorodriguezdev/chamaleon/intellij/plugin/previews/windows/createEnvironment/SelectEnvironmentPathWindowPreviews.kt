34de1359190da4c047042a223e22e9ce816bc65f
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-02-05 17:20:41 +0000

    Adds SelectEnvironmentPathWindow

--- intellij-plugin/standalone/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/previews/windows/createEnvironment/SelectEnvironmentPathWindowPreviews.kt
+++ intellij-plugin/standalone/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/previews/windows/createEnvironment/SelectEnvironmentPathWindowPreviews.kt
@@ -1,3 +1,20 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin.previews.windows.createEnvironment
 
-//TODO: Finish
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.previews.utils.PreviewContainer
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.windows.createEnvironment.SelectEnvironmentPathWindow
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.windows.createEnvironment.State
+
+@Preview
+@Composable
+internal fun SelectEnvironmentPathWindowPreview() {
+    PreviewContainer {
+        SelectEnvironmentPathWindow(
+            state = State.SelectEnvironmentPath(
+                "/location/location/location/location/location/location/location/location/",
+            ),
+            onSelectEnvironmentPathClicked = {}
+        )
+    }
+}
--- intellij-plugin/ui/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/ui.windows/createEnvironment/SelectEnvironmentPathWindow.kt
+++ intellij-plugin/ui/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/ui.windows/createEnvironment/SelectEnvironmentPathWindow.kt
@@ -1,9 +1,15 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.windows.createEnvironment
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.theme.LocalStrings
 import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.components.TooltipIconButton
 import io.github.gerardorodriguezdev.chamaleon.intellij.plugin.ui.windows.createEnvironment.State.SelectEnvironmentPath
+import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.TextField
 import org.jetbrains.jewel.ui.icons.AllIconsKeys
 
@@ -13,16 +19,29 @@ fun SelectEnvironmentPathWindow(
     state: SelectEnvironmentPath,
     onSelectEnvironmentPathClicked: () -> Unit,
 ) {
-    val textFieldState = rememberTextFieldState(initialText = state.name)
-    TextField(
-        state = textFieldState,
-        readOnly = true,
-        trailingIcon = {
-            TooltipIconButton(
-                iconKey = AllIconsKeys.Actions.NewFolder,
-                tooltip = state.name,
-                onClick = { onSelectEnvironmentPathClicked() }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(text = LocalStrings.current.environmentsDirectoryLocation, modifier = Modifier.widthIn(max = 140.dp))
+
+            val textFieldState = rememberTextFieldState(initialText = state.name)
+            TextField(
+                state = textFieldState,
+                readOnly = true,
+                trailingIcon = {
+                    TooltipIconButton(
+                        iconKey = AllIconsKeys.Actions.NewFolder,
+                        tooltip = state.name,
+                        onClick = { onSelectEnvironmentPathClicked() }
+                    )
+                },
             )
         }
-    )
+    }
 }
