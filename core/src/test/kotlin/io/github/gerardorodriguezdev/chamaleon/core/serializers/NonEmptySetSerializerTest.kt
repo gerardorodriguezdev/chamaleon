c22e7afeafda9a0e51e37eec7aa92c7a86263d53
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-02-09 15:36:30 +0000

    Adds non empty set tests

--- core/src/test/kotlin/io/github/gerardorodriguezdev/chamaleon/core/serializers/NonEmptySetSerializerTest.kt
+++ core/src/test/kotlin/io/github/gerardorodriguezdev/chamaleon/core/serializers/NonEmptySetSerializerTest.kt
@@ -1,6 +1,76 @@
 package io.github.gerardorodriguezdev.chamaleon.core.serializers
 
-//TODO: Finish
-class NonEmptySetSerializerTest {
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
+class NonEmptySetSerializerTest {
+    @Nested
+    inner class Serialize {
+        @Test
+        fun `GIVEN empty set WHEN serialize THEN throws error`() {
+            val intSet = IntSet(emptySet())
+
+            assertThrows<SerializationException> {
+                Json.encodeToJsonElement(intSet)
+            }
+        }
+
+        @Test
+        fun `GIVEN non empty set WHEN serialize THEN returns json`() {
+            //language=json
+            val expectedJson = """{"set":[1,2,3]}""".trimIndent()
+            val nullableString = IntSet(setOf(1, 2, 3))
+
+            val actualJson = Json.encodeToString(nullableString)
+
+            assertEquals(expected = expectedJson, actual = actualJson)
+        }
+    }
+
+    @Nested
+    inner class Deserialize {
+
+        @Test
+        fun `GIVEN json with empty set WHEN deserialize THEN throws error`() {
+            val json =
+                //language=json
+                """
+                {
+                    "set": []
+                }
+                """.trimIndent()
+
+            assertThrows<SerializationException> {
+                Json.decodeFromString<IntSet>(json)
+            }
+        }
+
+        @Test
+        fun `GIVEN valid non empty set WHEN deserialize THEN returns set`() {
+            val expectedSet = IntSet(setOf(1, 2, 3))
+            val json =
+                //language=json
+                """
+                {
+                    "set": [1,2,3]
+                }
+                """.trimIndent()
+
+            val actualSet = Json.decodeFromString<IntSet>(json)
+
+            assertEquals(expected = expectedSet, actual = actualSet)
+        }
+    }
+
+    @Serializable
+    private data class IntSet(
+        @Serializable(with = NonEmptySetSerializer::class)
+        val set: Set<Int>,
+    )
 }
