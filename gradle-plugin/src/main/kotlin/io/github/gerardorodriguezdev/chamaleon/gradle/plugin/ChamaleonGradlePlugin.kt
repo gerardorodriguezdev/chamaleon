43210045690cb591eb23e17e4cee8308d1514adc
Gerardo Rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-03-06 17:08:37 +0000

    Refactors task

--- gradle-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/gradle/plugin/ChamaleonGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/gradle/plugin/ChamaleonGradlePlugin.kt
@@ -2,25 +2,22 @@ package io.github.gerardorodriguezdev.chamaleon.gradle.plugin
 
 import io.github.gerardorodriguezdev.chamaleon.core.models.Project.Companion.ENVIRONMENTS_DIRECTORY_NAME
 import io.github.gerardorodriguezdev.chamaleon.core.results.ProjectDeserializationResult
-import io.github.gerardorodriguezdev.chamaleon.core.results.ProjectSerializationResult
 import io.github.gerardorodriguezdev.chamaleon.core.safeModels.ExistingDirectory
 import io.github.gerardorodriguezdev.chamaleon.core.safeModels.ExistingDirectory.Companion.toUnsafeExistingDirectory
 import io.github.gerardorodriguezdev.chamaleon.core.safeModels.NonEmptyString.Companion.toNonEmptyString
 import io.github.gerardorodriguezdev.chamaleon.core.serializers.ProjectDeserializer
-import io.github.gerardorodriguezdev.chamaleon.core.serializers.ProjectSerializer
 import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.extensions.ChamaleonExtension
 import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.mappers.toErrorMessage
 import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.tasks.GenerateSampleTask
+import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.tasks.SelectEnvironmentTask
 import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.tasks.generateEnvironment.GenerateEnvironmentTask
 import kotlinx.coroutines.runBlocking
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.file.Directory
 import org.gradle.api.tasks.TaskProvider
 
 public class ChamaleonGradlePlugin : Plugin<Project> {
-    private val projectSerializer = ProjectSerializer.create()
     private val projectDeserializer = ProjectDeserializer.create()
 
     override fun apply(target: Project) {
@@ -62,7 +59,6 @@ public class ChamaleonGradlePlugin : Plugin<Project> {
     private fun Project.environmentsExistingDirectory(): ExistingDirectory =
         layout.projectDirectory.dir(ENVIRONMENTS_DIRECTORY_NAME).asFile.toUnsafeExistingDirectory()
 
-
     private fun Project.registerGenerateSampleTask(): TaskProvider<GenerateSampleTask> =
         tasks.register(GENERATE_SAMPLE_TASK_NAME, GenerateSampleTask::class.java) {
             val generateSampleCommandLineArgument =
@@ -77,40 +73,19 @@ public class ChamaleonGradlePlugin : Plugin<Project> {
             )
         }
 
-    //TODO: Move if possible + logs
-    private fun Project.registerSelectEnvironmentTask(extension: ChamaleonExtension): TaskProvider<Task> =
-        tasks.register(SELECT_ENVIRONMENT_TASK_NAME) {
-            val newSelectedEnvironmentName = providers.gradleProperty(SELECT_ENVIRONMENT_COMMAND_LINE_ARGUMENT).orNull
+    private fun Project.registerSelectEnvironmentTask(extension: ChamaleonExtension): TaskProvider<SelectEnvironmentTask> =
+        tasks.register(SELECT_ENVIRONMENT_TASK_NAME, SelectEnvironmentTask::class.java) {
+            val newSelectedEnvironmentNameString =
+                providers.gradleProperty(SELECT_ENVIRONMENT_COMMAND_LINE_ARGUMENT).orNull
 
-            doLast {
-                val newSelectedEnvironmentName =
-                    if (newSelectedEnvironmentName == null) {
-                        null
-                    } else {
-                        newSelectedEnvironmentName.toNonEmptyString()
-                            ?: throw ChamaleonGradlePluginException("Selected environment name was empty")
-                    }
-
-                val currentProject = extension.project.get()
-
-                val newProject = currentProject.updateProperties(
-                    newSelectedEnvironmentName = newSelectedEnvironmentName,
-                )
-
-                if (newProject == null) {
-                    throw ChamaleonGradlePluginException("Selected environment not found on existing environments ${currentProject.environments}")
-                }
-
-                runBlocking {
-                    val updateProjectResult = projectSerializer.serialize(newProject)
-                    when (updateProjectResult) {
-                        is ProjectSerializationResult.Success -> logger.info("Environment selected successfully")
-                        is ProjectSerializationResult.Failure -> throw ChamaleonGradlePluginException(
-                            message = updateProjectResult.toErrorMessage()
-                        )
-                    }
-                }
+            if (newSelectedEnvironmentNameString == null) {
+                newSelectedEnvironmentName.set(null)
+            } else {
+                newSelectedEnvironmentNameString.toNonEmptyString()
+                    ?: throw ChamaleonGradlePluginException("Selected environment name was empty")
             }
+
+            project.set(extension.project.get())
         }
 
     private fun Project.registerGenerateEnvironmentTask(): TaskProvider<GenerateEnvironmentTask> =
---
+++ gradle-plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/gradle/plugin/tasks/SelectEnvironmentTask.kt
@@ -0,0 +1,57 @@
+package io.github.gerardorodriguezdev.chamaleon.gradle.plugin.tasks
+
+import io.github.gerardorodriguezdev.chamaleon.core.models.Project
+import io.github.gerardorodriguezdev.chamaleon.core.results.ProjectSerializationResult
+import io.github.gerardorodriguezdev.chamaleon.core.safeModels.NonEmptyString
+import io.github.gerardorodriguezdev.chamaleon.core.serializers.ProjectSerializer
+import io.github.gerardorodriguezdev.chamaleon.gradle.plugin.mappers.toErrorMessage
+import kotlinx.coroutines.runBlocking
+import org.gradle.api.DefaultTask
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+
+@CacheableTask
+public abstract class SelectEnvironmentTask : DefaultTask() {
+    private val projectSerializer = ProjectSerializer.create()
+
+    @get:Input
+    public abstract val newSelectedEnvironmentName: Property<NonEmptyString?>
+
+    @get:Input
+    public abstract val project: Property<Project>
+
+    @TaskAction
+    public fun selectEnvironment() {
+        val newSelectedEnvironmentName = newSelectedEnvironmentName.get()
+        val currentProject = project.get()
+
+        val newProject = currentProject.updateProperties(
+            newSelectedEnvironmentName = newSelectedEnvironmentName,
+        )
+
+        if (newProject == null) {
+            throw SelectEnvironmentTaskException(
+                message = "Selected environment not found on existing environments ${currentProject.environments}",
+            )
+        }
+
+        newProject.serialize()
+    }
+
+    private fun Project.serialize() =
+        runBlocking {
+            when (val updateProjectResult = projectSerializer.serialize(this@serialize)) {
+                is ProjectSerializationResult.Success ->
+                    logger.info("Environment selected successfully at '${environmentsDirectory.path}'")
+
+                is ProjectSerializationResult.Failure ->
+                    throw SelectEnvironmentTaskException(
+                        message = updateProjectResult.toErrorMessage(),
+                    )
+            }
+        }
+
+    private class SelectEnvironmentTaskException(message: String) : Exception(message)
+}
