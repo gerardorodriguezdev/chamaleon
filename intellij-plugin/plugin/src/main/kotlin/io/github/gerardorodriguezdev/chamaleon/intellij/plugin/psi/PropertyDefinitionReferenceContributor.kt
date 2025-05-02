cc3de94881cd943da4bb2f8cbcf5b1c7bb4a8f90
gerardo.rodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2025-05-02 09:46:57 +0100

    Adds file pattern

--- intellij-plugin/plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/psi/PropertyDefinitionReferenceContributor.kt
+++ intellij-plugin/plugin/src/main/kotlin/io/github/gerardorodriguezdev/chamaleon/intellij/plugin/psi/PropertyDefinitionReferenceContributor.kt
@@ -1,6 +1,7 @@
 package io.github.gerardorodriguezdev.chamaleon.intellij.plugin.psi
 
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiFile
 import com.intellij.psi.PsiReferenceContributor
 import com.intellij.psi.PsiReferenceRegistrar
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
@@ -8,8 +9,14 @@ import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 internal class PropertyDefinitionReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         registrar.registerReferenceProvider(
-            PlatformPatterns.psiElement(KtStringTemplateExpression::class.java),
+            PlatformPatterns
+                .psiElement(KtStringTemplateExpression::class.java)
+                .inFile(psiFile().withName(BUILD_GRADLE_FILE_NAME)),
             PropertyDefinitionReferenceProvider(),
         )
+    }
+
+    private companion object {
+        const val BUILD_GRADLE_FILE_NAME = "build.gradle.kts"
     }
 }
